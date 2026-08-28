5d24e791dcc29f3f92b0b0902b0ab6bdf4195a1d
Sushant Ray <goodboyneon03@gmail.com>
2026-08-28 13:27:57 +0545

    refactor: remove unwanted authentication files

--- src/lib/sign-in.ts
+++
@@ -1,9 +0,0 @@
-import { authClient } from "@/lib/auth-client"; //import the auth client
-
-await authClient.signIn.social({
-  provider: "discord",
-  callbackURL: "/",
-  errorCallbackURL: "/error",
-  newUserCallbackURL: "/",
-  disableRedirect: false,
-});
